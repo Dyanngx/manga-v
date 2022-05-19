0920efe5130cbd17bc737775d0d446c5b1ab2032
Dyanngx <Dyanngx@gmail.com>
2022-05-19 12:22:45 +0700

    update narbar code

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {MenuItems} from "./MenuItems";
+import { Button } from "../Button";
 import './Navbar.css';
 
 class Navbar extends Component{
@@ -29,6 +30,7 @@ class Navbar extends Component{
                         )
                     })}
                 </ul>
+                <Button>Sign up</Button>
             </nav>
         )
     }
